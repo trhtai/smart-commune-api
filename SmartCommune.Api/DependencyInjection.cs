e4b0dc80d1f1a06c4d428a30f4b4843ddf94753b
Trương Hữu Tài <taitruong9402@gmail.com>
2025-12-26 15:32:18 +0700

    Add project files.

---
+++ SmartCommune.Api/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartCommune.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+
+        return services;
+    }
+}
